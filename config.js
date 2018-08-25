fe11831618d97146f1b7aeae652a00e504068131
Mathieu Andrade <26581253+MathieuAndrade@users.noreply.github.com>
2018-08-25 22:56:26 +0200

    Fix missing config require in init.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     address: process.env.DEVICE_ADRDRESS || '', // The adresse of your ps4
-    creds: process.env.CREDS || 'D:\Lab\Lab Node JS\PS4-module\ps4-creds.json', 
+    creds: process.env.CREDS || '', // Path of your credential
 };
